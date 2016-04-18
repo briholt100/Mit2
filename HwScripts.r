d557d5f331191a34f9f274d56f170970eea83e69
Brian Holt <brian.c.holt@gmail.com>
2016-04-17 19:24:07 -0700

    section2

--- HwScripts.r
+++ HwScripts.r
@@ -5,6 +5,18 @@
 wrkdir<-'/home/brian/Projects/Mit2'
 
 setwd(wrkdir)
+
+WHO<-read.csv('./data/WHO.csv')
+summary(WHO)
+
+sort((WHO$Over60))
+WHO$Coun[WHO$Over60<1]
+sort((WHO$LiteracyRate))
+(WHO[WHO$LiteracyRate>99,c(1,10)])
+tapply(WHO$ChildMortality,WHO$Region,mean)
+
+
+#  Analytic detective
 mvt<-read.csv('./data/mvtWeek1.csv')
 str(mvt)
 max(mvt$ID)
@@ -36,4 +48,36 @@ for(i in 1:nrow(tbl)){
 
 table(Top5$Weekday,Top5$Loc=="GAS STATION")
 table(Top5$Weekday,Top5$Loc=="DRIVEWAY - RESIDENTIAL")
-    #Week 2
+
+
+#stock market
+IBM<-read.csv('./data/IBMStock.csv')
+GE<-read.csv('./data/GEStock.csv')
+CocaCola<-read.csv('./data/CocaColaStock.csv')
+ProcterGamble<-read.csv('./data/ProcterGambleStock.csv')
+Boeing<-read.csv('./data/BoeingStock.csv')
+str(GE)
+
+# convert date variable from factor
+
+IBM$Date = as.Date(IBM$Date, "%m/%d/%y")
+GE$Date = as.Date(GE$Date, "%m/%d/%y")
+CocaCola$Date = as.Date(CocaCola$Date, "%m/%d/%y")
+ProcterGamble$Date = as.Date(ProcterGamble$Date, "%m/%d/%y")
+Boeing$Date = as.Date(Boeing$Date, "%m/%d/%y")
+stocks<-c('IBM','GE','CocaCola','ProcterGamble','Boeing')
+
+lapply(stocks,function(x){max(x[,2])})
+lapply(stocks,function(x){sd(x[[2]])})
+
+mean(IBM$StockPrice)
+min(GE$StockPrice)
+max(CocaCola$StockPrice)
+
+
+plot(x=CocaCola$Date,CocaCola$StockPrice,pch=19,cex=.2, col=4, lty=4)
+abline(h=min(CocaCola$StockPrice), col='red')
+
+lines(ProcterGamble$Date, ProcterGamble$StockPrice, col='black',lty=2)
+abline(h=min(ProcterGamble$StockPrice), col='green')
+#Week 2
