dab3f7658fc00a32b53201769705320475c8fbd0
Brian Holt <brian.c.holt@gmail.com>
2016-05-28 23:28:56 -0700

    getting started needed changes to work

---
+++ kaggle/KCompetition_GettingStarted.R
@@ -0,0 +1,45 @@
+# KAGGLE COMPETITION - GETTING STARTED
+
+# This script file is intended to help you get started on the Kaggle platform, and to show you how to make a submission to the competition.
+
+
+# Let's start by reading the data into R
+# Make sure you have downloaded these files from the Kaggle website, and have navigated to the directory where you saved the files on your computer
+#Load data
+school<-"I:/My Data Sources/mooc/MitAnalytic"
+setwd(school)
+home<-getwd()
+setwd(paste0(home, "/mooc/MitAnal"))
+dater<-getwd()
+setwd(paste0(dater, "/Mit2"))
+dir()
+
+train = read.csv("./kaggle/train2016.csv")
+
+test = read.csv("./kaggle/test2016.csv")
+str(test)
+# We will just create a simple logistic regression model, to predict Party using all other variables in the dataset, except for the user ID:
+
+SimpleMod = glm(Party ~ . -USER_ID, data=train, family=binomial)
+summary(SimpleMod)
+# And then make predictions on the test set:
+PredTrain = predict(SimpleMod,newdata=train, type="response")
+PredTest = predict(SimpleMod, newdata=test, type="response")
+
+threshold = 0.5
+PredTrainLabels = as.factor(ifelse(PredTrain<threshold, "Democrat", "Republican"))
+PredTestLabels = as.factor(ifelse(PredTest<threshold, "Democrat", "Republican"))
+
+tbl<-table(train$Party,PredTrain>.5)
+(tbl[1]+tbl[4])/sum(tbl)
+# However, you can submit the file on Kaggle to see how well the model performs. You can make up to 5 submissions per day, so don't hesitate to just upload a solution to see how you did.
+
+# Let's prepare a submission file for Kaggle (for more about this, see the "Evaluation" page on the competition site):
+
+MySubmission = data.frame(USER_ID = test$USER_ID, Predictions = PredTestLabels)
+
+write.csv(MySubmission, "./kaggle/SubmissionSimpleLog.csv", row.names=FALSE)
+
+# You should upload the submission "SubmissionSimpleLog.csv" on the Kaggle website to use this as a submission to the competition
+
+# This model was just designed to help you get started - to do well in the competition, you will need to build better models!
