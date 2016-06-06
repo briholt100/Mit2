4d0d1a6f6ec2d2eac98bf49306b55e417feeaa93
Brian Holt <brian.c.holt@gmail.com>
2016-06-05 17:54:25 -0700

    preprocessing data

--- kaggle/Explore_Kaggle.r
+++ kaggle/Explore_Kaggle.r
@@ -17,22 +17,22 @@ dir()
 
 kaggle_train <- read.csv("./kaggle/train2016.csv",stringsAsFactors=T,na.strings = c("NA", '',' '))
 kaggle_test <- read.csv("./kaggle/test2016.csv",stringsAsFactors=T,na.strings = c("NA", '',' '))
-str(kaggle_train)
-str(kaggle_test)
 
 #join test and train
 train<-kaggle_train
 test<-kaggle_test
-trainParty<-train$Party
-train<-train[,-7]
-train$Party<-trainParty
 test$Party<-NA
+trainParty<-train$Party
+train<-train[,-7]  #step 1 to move 'party' to end
+train$Party<-trainParty #step 2 to move party to end
+train$data_set<-"train"
+test$Party<-NA
+test$data_set<-"test"
 length(test)
 df<-rbind(train,test)
 
 
-#Explore and clean
-
+#Explore, clean, create variables
 df$sum_na<-apply(df,1,function(x) sum(is.na(x))) #adds count variable of NA
 
 #convert YOB to date
@@ -53,49 +53,65 @@ points(y=df$sum_na[is.na(df$Party)],x=df$age[is.na(df$Party)],col="green")
 
 df[which(df$age>80),c('age',"Gender","Income","HouseholdStatus","EducationLevel","Party")]
 
+tail(sort(df$YOB),20)
+
+summary(df$YOB)
+plot(x=df$USER_ID,df$YOB,pch=19, col='blue',cex=.1)
+abline(h=2000,col='green')
+abline(h=1925,col='red')
+points(x=df$USER_ID[df$YOB>='2003'],y=df$YOB[df$YOB>='2003'],col="black",cex=.3,pch=3)
+points(x=df$USER_ID[df$YOB<'1925'],y=df$YOB[df$YOB<'1925'],col="green",cex=.3,pch=3)
+points(x=df$USER_ID[is.na(df$YOB)==T],y=df$YOB[is.na(df$YOB)==T],col="orange",cex=.3,pch=3)
+
+for(i in 1:length(df$YOB)){
+  if (!is.na(df$YOB[i])){
+    if(df$YOB[i] < 1925 | df$YOB[i]>2003) {
+      df$YOB[i]<-NA
+      print(df$YOB[i])
+    }
+  }
+}
+
+
+#relevel
+#######for df
+df$Income<-relevel(df$Income,ref="over $150,000")
+df$Income<-relevel(df$Income,ref="$100,001 - $150,000")
+df$Income<-relevel(df$Income,ref="$75,000 - $100,000")
+df$Income<-relevel(df$Income,ref="$50,000 - $74,999")
+df$Income<-relevel(df$Income,ref="$25,001 - $50,000")
+df$Income<-relevel(df$Income,ref="under $25,000")
+
+levels(df$EducationLevel)
+df$EducationLevel<-relevel(df$EducationLevel,ref="Current K-12")
+df$HouseholdStatus<-relevel(df$HouseholdStatus,ref="Single (no kids)")
+
+
+
+
+
+
+#return df back to test and train, split train set
+
+train<-df[which(df$data_set=='train'),]
+test<-df[which(df$data_set!='train'),]
+nrow(test)
+
+set.seed(144)
+spl = sample(1:nrow(train), size=0.7*nrow(train))
+train_df = train[spl,]
+test_df = train[-spl,]
+
+
+
+
 
 #df<-df[complete.cases(df)==T,]  # read.csv na.strings = '' complete cases = 697
-
 
 install.packages("mice")
 library(mice)
 
-# Multiple imputation
-#set.seed(144)
-summary(df)
-imputed = complete(mice(df[,2:110]))
-summary(imputed)
-df[,2:110] = imputed
-summary(df)
-write.csv(df, "df.csv", row.names=FALSE)
 
-
-
-
-
-set.seed(144)
-spl = sample(1:nrow(train), size=0.7*nrow(train))
-train = train[spl,]
-test = train[-spl,]
-
-
-
-
-
-#convert blanks "" into "blank"; must read.csv stringsAsFactors=F
-train[train=='']<-'blank'
-test[test=='']<-'blank'
-#convert to factor
-train[,3:108]<-lapply(train[,3:108],factor)
-test[,3:108]<-lapply(test[,3:108],factor)
-
-apply(train,2,(table))
-boxplot(train$YOB)
-summary(train$YOB)
-par(mfrow=c(1,1))
-boxplot(train$age~train$Party)
-
-table(train$Gender,train$Party,is.na(train$age))
 
 
 
@@ -104,7 +120,7 @@ table(train$Gender,train$Party,is.na(train$age))
 
 # We will just create a simple logistic regression model, to predict Party using all other variables in the dataset, except for the user ID:
 
-SimpleMod = glm(Party ~ . -USER_ID -YOB, data=train, family=binomial)
+SimpleMod = glm(Party ~ . -USER_ID -YOB, data=train_df, family=binomial)
 summary(SimpleMod)
 # And then make predictions on the test set:
 PredTrain = predict(SimpleMod,newdata=train, type="response")
@@ -150,3 +166,24 @@ MySubmissionCart = data.frame(USER_ID = test$USER_ID, Predictions = predCart1)
 
 write.csv(MySubmissionCart, "./kaggle/SubmissionSimpleCart.csv", row.names=FALSE)
 
+
+
+
+# Multiple imputation for all variables, including test set, with no preprocessing other than adding 'age' and sum_na.
+#set.seed(144)
+
+
+summary(df)
+imputed = complete(mice(df[,2:110]))
+summary(imputed)
+imputed$USER_ID <-df$USER_ID
+summary(imputed)
+write.csv(imputed, "./kaggle/Full_imputed.csv", row.names=FALSE)
+
+temp<-merge(test,imputed,by.x="USER_ID",by.y="USER_ID",all.x=T)
+
+MySubmissionFull_Imput <- data.frame(USER_ID = test$USER_ID, Predictions = temp$Party.y)
+
+write.csv(MySubmissionFull_Imput, "./kaggle/MySubmissionFull_Imput.csv", row.names=FALSE)
+
+#this does not work well.
